1549966ab8b85a2e8dbf7cb69c909f487f49e414
alessandroCiciarelli <36853940+alessandroCiciarelli@users.noreply.github.com>
2021-07-15 11:07:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ import os
  
  
 if submit_button:
-    os.system("pip install instagram-scraper")
+    
     if not os.path.exists(f'{username}'):
-        os.system(f'instagram-scraper "{username}" --profile-metadata  --media-metadata  --media-types none')
+        os.system(f'instagram-scraper "{username}" --profile-metadata  --media-metadata  --media-types -u sharks_business_holding -p Al260900.')
     try:
         print("ok")
         js = json.load(open(f'{username}/{username}.json', encoding='utf-8'))
